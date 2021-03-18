78a343dbc082b0ed6ee43e2a7bfc4e8f671ebb46
Edilene Shizue <edilene.shizue@aluno.ifsp.edu.br>
2021-03-18 13:15:12 -0300

    Projeto de desenvolvimento web I - Prefeitura de Peruíbe

---
+++ ProjetoWebI/Projeto web I/Projeto web I/js/script.js
@@ -0,0 +1,5 @@
+//Este código captura o horário local
+		let d = new Date();
+		document.body.innerHTML = "<h1>Horário de Brasília:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
+		"</h1>"
+	
